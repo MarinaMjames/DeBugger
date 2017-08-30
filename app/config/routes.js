9e426b44da18c5b72d764cc8f76dd8bdce52f941
MarinaMJames <marinajames93@gmail.com>
2017-08-29 21:41:54 -0400

    added code to app.js and routes.js

---
+++ app/config/routes.js
@@ -0,0 +1,34 @@
+// Inclue the React library
+var React = require("react");
+
+// Include the react-router module
+var router = require("react-router");
+
+// Include the Route component for displaying individual routes
+var Route = router.Route;
+
+// Include the Router component to contain all our Routes
+// Here where we can pass in some configuration as props
+var Router = router.Router;
+
+var browserHistory = router.browserHistory;
+
+// Include the IndexRoute (catch-all route)
+var IndexRoute = router.IndexRoute;
+
+// Reference the high-level components
+var Main = require("../components/Main");
+
+// Export the Routes
+module.exports = (
+
+  // The high level component is the Router component
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+        <Route path="" component={} />
+        <Route path="" component={} />
+
+        <IndexRoute component={} />
+    </Route>
+  </Router>
+);
