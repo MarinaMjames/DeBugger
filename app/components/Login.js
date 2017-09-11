d4ce9707c03ed7906a682f601491a9154c02d429
Joseph Ball <jball321@live.com>
2017-09-10 21:04:27 -0400

    Sign up form and login form and their states.

--- app/components/Login.js
+++ app/components/Login.js
@@ -4,23 +4,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Login extends React.Component{
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
   // Here we render the function
   render() {
 
     return (
       <div className="col-xs-12 col-md-12">
-        <div className="field">
-          <div className="control">
-            <input className="input is-info" type="text" placeholder="Username"/>
+        <form onSubmit={this.onSubmit}>
+          <div className="field">
+            <div className="control">
+              <input className="input is-info" type="text" placeholder="Username" name="username" value={this.state.username} onChange={this.onChange}/>
+            </div>
           </div>
-        </div>
-        <div className="field">
-          <div className="control">
-            <input className="input is-info" type="password" placeholder="Password"/>
+          <div className="field">
+            <div className="control">
+              <input className="input is-info" type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
+            </div>
           </div>
-        </div>
-      	<button className="button">Login</button>
+        	<button className="button">Login</button>
+        </form>
       </div>
     )
   }
--- app/components/Signup.js
+++ app/components/Signup.js
@@ -4,33 +4,57 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Signup extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      username: '',
+      password: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
 
   // Here we render the function
   render() {
 
     return (
       <div className="col-xs-12 col-md-12">
-        <div className="field">
-          <div className="control">
-            <input className="input is-info" type="text" placeholder="Full Name"/>
-          </div>
-        </div>
-        <div className="field">
-          <div className="control">
-            <input className="input is-info" type="text" placeholder="E-mail"/>
-          </div>
-        </div>
-        <div className="field">
-          <div className="control">
-            <input className="input is-info" type="text" placeholder="Username"/>
-          </div>
-        </div>
-        <div className="field">
-          <div className="control">
-            <input className="input is-info" type="password" placeholder="Password"/>
-          </div>
-        </div>
-        <button className="button">Create Account</button>
+        <form onSubmit={this.onSubmit}>
+            <div className="field">
+              <div className="control">
+                <input className="input is-info" type="text" placeholder="Full Name" name="name" value={this.state.name} onChange={this.onChange}/>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input className="input is-info" type="text" placeholder="E-mail" name="email" value={this.state.email} onChange={this.onChange}/>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input className="input is-info" type="text" placeholder="Username" name="username" value={this.state.username} onChange={this.onChange}/>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input className="input is-info" type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
+              </div>
+            </div>
+            <button className="button">Create Account</button>
+        </form>
       </div>
     )
   }
