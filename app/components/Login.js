66600c38907e42dca4ed670d62bdbb7e7470bfef
Marina James <marinajames93@gmail.com>
2017-09-12 22:18:49 -0400

    Login page fixes

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,7 +1,75 @@
+// Include React
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+import helpers from '../utils/helpers';
+
+export default class Login extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      errors: {},
+      user: {
+        username: '',
+        password: ''
+      },
+      isLoading: false
+    }
+
+    // bind component functions
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  // get user inputs and save them to state
+  onChange(e) {
+    const field = e.target.name;
+    const user = this.state.user;
+    user[field] = e.target.value;
+    this.setState({ user });
+  }
+
+  // submit sign up form
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({ errors: {}, isLoading: true });
+    helpers.userLogin(this.state.user).then((data) => {
+      // if errors are returned save them to state
+      if (!data.success) {
+        this.setState({ errors: data.errors, isLoading: false });
+      } else {
+      // else if user was successfully created proceed to login form
+        this.props.history.push('/');
+      }
+    });
+  }
+  // render component
+  render() {
+
+    return (
 
       <div className="form col-xs-12 col-md-12">
+        <form onSubmit={this.onSubmit}>
+          {this.state.errors.message && <p className="help is-danger">{this.state.errors.message}</p>}
+          <div className="field">
+            <div className="control">
+              <input
+                className="input is-info"
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={this.state.user.username}
+                onChange={this.onChange}
+              />
+              {this.state.errors.username && <p className="help is-danger">{this.state.errors.username}</p>}
+            </div>
 
+          </div>
+          <div className="field">
+            <div className="control">
+              <input
+                className="input is-info"
                 type="password"
                 placeholder="Password"
                 name="password"
