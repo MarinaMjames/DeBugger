d4c62c7f9d87965951d2a25309582048b35c1f40
Marina James <marinajames93@gmail.com>
2017-09-12 20:11:19 -0400

    styled the login page and added the navbar to the game page

--- app/components/Login.js
+++ app/components/Login.js
@@ -9,7 +9,7 @@ export default class Login extends React.Component{
   render() {
 
     return (
-      <div className="col-xs-12 col-md-12">
+      <div className="form col-xs-12 col-md-12">
         <div className="field">
           <div className="control">
             <input className="input is-info" type="text" placeholder="Username"/>
